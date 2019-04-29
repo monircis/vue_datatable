6f18f5308f8ccdfb8df50795c4ce19984c2a138e
Monirul islam <monirjss@gmail.com>
2019-04-29 12:29:57 +0600

    on offf status  with  loader specific  show

--- src/components/server_lists.vue
+++ src/components/server_lists.vue
@@ -25,46 +25,35 @@
           <td>{{ droplet.name }}</td>
           <td>{{ droplet.id}}</td>
           <td width="50px" class="status">
-            <div class="show-delay" v-if="delay" v-model="delay">
+            <div class="show-delay" :id="'id_' + index" v-show="delay">
               <img src="@/assets/images/circle-loader.gif"/>
             </div>
-            <div v-if="droplet.state=='off'">
-              <button type="submit" class="btn btn-danger" @click="updateStatus(index)" v-model="droplet.state">OFF
-              </button>
-            </div>
-            <div v-else>
-              <div v-if="droplet.state=='starting'">
-                <button type="submit" class="btn btn-info" @click="updateStatus(index)" v-model="droplet.state">ON
-                </button>
-              </div>
-              <div v-else>
-                <button type="submit" class="btn btn-info" @click="updateStatus(index)" v-model="droplet.state">ON
-                </button>
-              </div>
-            </div>
-            <!--<div class="switch" v-if="droplet.state==='off'">-->
-            <!--<label>-->
-            <!--OFF {{droplet.state }}-->
-            <!--<input type="checkbox"  @change="updateStatus(index)" checked="false">-->
-            <!--<span class="lever"></span> ON-->
-            <!--</label>-->
-            <!--</div>-->
-            <!--<div class="switch" v-else>-->
-            <!--<label>-->
-            <!--OFF {{droplet.state }}-->
-            <!--<input type="checkbox"  @change="updateStatus(index)" checked="true">-->
-            <!--<span class="lever"></span> ON-->
-            <!--</label>-->
-            <!--</div>-->
 
-            <!--<div class="switch" v-else-if="droplet.state == 'ready'">-->
-            <!--<label>-->
-            <!--OFF {{ droplet.state }}-->
-            <!--<input type="checkbox" checked  v-model="droplet.state"  @change="updateStatus(index)">-->
-            <!--<span class="lever"></span> ON-->
-            <!--</label>-->
+            <div class="switch">
+            <label>
+            OFF
+            <input type="checkbox" v-model="droplet.state"  @change="updateStatus(index,$event)">
+            <span class="lever"></span> ON
+            </label>
+            </div>
+
+            <!--<div v-if="droplet.state=='off'">-->
+            <!--<button type="submit" class="btn btn-danger" v-on:click="updateStatus(index,$event)" v-model="droplet.state">OFF-->
+            <!--</button>-->
             <!--</div>-->
-            <!--<button type="button" class="btn btn-sm  btn-info waves-effect waves-light">ON/OFF</button>-->
+            <!--<div v-else>-->
+            <!--<div class="show-delay" :id="'id_' + index" v-show="delay">-->
+            <!--<img src="@/assets/images/circle-loader.gif"/>-->
+            <!--</div>-->
+            <!--<div v-if="droplet.state=='starting'">-->
+            <!--<button type="submit" class="btn btn-info" v-on:click="updateStatus(index,$event)" v-model="droplet.state">ON-->
+            <!--</button>-->
+            <!--</div>-->
+            <!--<div v-else>-->
+            <!--<button type="submit" class="btn btn-info" v-on:click="updateStatus(index,$event)" v-model="droplet.state">ON-->
+            <!--</button>-->
+            <!--</div>-->
+            <!--</div>-->
           </td>
           <td width="120px">
             <router-link class="btn btn-configure btn-sm" v-bind:to="'droplet-details/'+droplet.id">Configure
@@ -81,12 +70,19 @@
 
 <script>
   import axios from 'axios';
-
+  const $ = require('jquery');
+  window.$ = $;
+  //click a date and  catch  the date
+  $(function () {
+    // $('input').click(function(){
+    //   $(".show-delay").addClass("intro");
+    //   alert();
+    // });
+  });
   export default {
     data() {
       return {
         delay: false,
-        status: '',
         searchDroplet: '',
         serverStatus: '',
         droplets: [],
@@ -105,8 +101,13 @@
         axios.get("https://command-center-apis.herokuapp.com/vm/")
           .then(function (response) {
             //console.log(response.data);
-            vm.droplets = response.data.data;
-            vm.alldroplets = response.data.data;
+            let tempData= response.data.data;
+            tempData.forEach(function (droplet) {
+              droplet.state=droplet.state === 'ready';
+              vm.droplets.push(droplet);
+            });
+            //vm.droplets = response.data.data;
+            vm.alldroplets = vm.droplets;
             //end loading
             vm.$parent.endLoading();
           })
@@ -115,66 +116,48 @@
             //console.log(response);
           });
       },
-      updateStatus: function (index) {
-        this.delay = true;
+      updateStatus: function (index,event) {
+        //make  show  loader  just specific  button
+        let loader = $(event.target).parent().parent().siblings('#id_'+index);
+        $(loader).css({"display": "block"});
         let vm = this;
+        //vm.delay=true;
         //get  ingle  droplet by  index
         let getdroplet = this.droplets[index];
+
         //console.log(getdroplet);
         //find state
         let state = getdroplet.state;
         //alert(state);
         let dropletID = getdroplet.id;
-        if (state == 'off') {
-          console.log(state);
-          //axios.get("https://command-center-apis.herokuapp.com/vm/start/"+dropletID+'/')
-          axios.put("https://command-center-apis.herokuapp.com/vm/start/" + dropletID + '/', {headers: {"Content-Type": 'application/json'}})
+        if (state==true) {
+          //alert(dropletID);
+          //console.log(state);
+          axios.put("https://command-center-apis.herokuapp.com/vm/start/" + dropletID + '/', {headers: {"Content-Type": 'application/json'}, timeout: 2*60*1000})
             .then(function (response) {
-              //vm.$toasted.show('Success', {type: 'success', icon: 'fa-exclamation-triangle'});
-              //end loading
-              vm.$parent.endLoading();
-              vm.$router.go();
-              vm.droplets.$set(index, {state: 'ready'});
+              console.log(response.data.data);
+              vm.$toasted.show('Server Is ON :: '+getdroplet.name, {type: 'success', icon: 'fa-exclamation-triangle'});
+              // show false loader
+              $(loader).css({"display": "none"});
             })
             .catch(function (error) {
-              //vm.$toasted.show('Success', {type: 'success', icon: 'fa-exclamation-triangle'});
-              //end loading
-              vm.$parent.endLoading();
-              vm.$router.go();
-              vm.droplets.$set(index, {state: 'ready'});
-            });
-          //alert("i am  true"+ dropletID);
-        } else {
-          console.log(state);
-          //vm.$parent.startLoading();
-          //axios.get("https://command-center-apis.herokuapp.com/vm/stop/"+dropletID+'/')
-          // axios.put("https://command-center-apis.herokuapp.com/vm/stop/psygfb9zd/",{headers: { "Content-Type": 'application/json'}})
+            vm.$toasted.show('Server Is ON :: '+getdroplet.name, {type: 'success', icon: 'fa-exclamation-triangle'});
+              console.log(error);
+              // show false loader
+              $(loader).css({"display": "none"});
+              });
+        }else{
           axios.put("https://command-center-apis.herokuapp.com/vm/stop/" + dropletID + '/', {headers: {"Content-Type": 'application/json'}})
             .then(function (response) {
-              //vm.$toasted.show('Success', {type: 'success', icon: 'fa-exclamation-triangle'});
-              //end loading
-              //vm.$parent.endLoading();
-              vm.$router.go();
-              vm.droplets.$set(index, {state: 'off'});
+              vm.$toasted.show('Server Is OFF  :: '+getdroplet.name, {type: 'error', icon: 'fa-exclamation-triangle'});
               console.log(this.droplets);
-              //console.log(response.data);
-              // vm.droplets.$remove(index)
-              //reload this  page
-              //vm.$router.go();
-
+              // show false loader
+              $(loader).css({"display": "none"});
             })
             .catch(function (error) {
-              //end loading
-              //vm.$parent.endLoading();
-              //vm.$toasted.show('Success', {type: 'success', icon: 'fa-exclamation-triangle'});
-              vm.$router.go();
-              vm.droplets.$set(index, {state: 'off'});
-
+              $(loader).css({"display": "none"});
             });
-          //alert("i am  false"+ dropletID);
         }
-        //console.log(this.droplets[index]);
-        //alert();
       }
     },
     watch: {
